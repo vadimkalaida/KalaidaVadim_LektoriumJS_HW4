83f17a16d137819b0de27ab83ac2f837192277a0
VadimKalayda <vadosgta1@gmail.com>
2019-10-31 13:18:39 +0200

    some changes

--- Task4/script/index.js
+++ Task4/script/index.js
@@ -1,50 +1,3 @@
-Array.prototype.myForeach = function(callback, i) {
-  if(i !== undefined) {
-    for(; i < this.length; i++) {
-      callback(this[i]);
-    }
-  } else {
-    for(let i = 0; i < this.length; i++) {
-      callback(this[i]);
-    }
-  }
-};
-
-Array.prototype.myMap = function (callback, i) {
-  let resultArr = [];
-  if(i !== undefined) {
-    for(; i < this.length; i++) {
-      resultArr.push(callback(this[i], i, this));
-    }
-  } else {
-    for(let i = 0; i < this.length; i++) {
-      resultArr.push(callback(this[i], i, this));
-    }
-  }
-  return resultArr;
-};
-
-Array.prototype.mySort = function(compareFunc) {
-  if(compareFunc !== undefined) {
-    return compareFunc();
-  } else {
-    for(let i = 0; i < this.length; i++) {
-
-      for(let j = i + 1; j < this.length; j++) {
-
-        if(this[i] > this[j]) {
-          let swap = this[i];
-          this[i] = this[j];
-          this[j] = swap;
-        }
-
-      }
-    }
-    return this;
-  }
-
-};
-
 Array.prototype.myPush = function(val) {
   this[this.length] = val;
   return this;
@@ -82,8 +35,5 @@ function task(R, C, r0, c0) {
 
 
 console.log('\'*\' - required');
-console.log('\'arr.myForeach(*callback function, index)\' - foreach');
-console.log('\'arr.myMap(*callback(val, index, array), index)\' - map');
-console.log('\'arr.mySort(compareFunc)\' - sort');
-console.log('\'arr.myPush(Value)\' - push');
-console.log('\'task(R, C, r0, c0)\' - push');
+console.log('\'arr.myPush(*Value)\' - push');
+console.log('\'task(*R, *C, *r0, *c0)\' - push');
